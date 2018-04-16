2858ca52d05ab4e0f52f242c72412cf63642eada
Yaneury Fermin <fermin.yaneury@gmail.com>
2018-04-15 20:01:34 -0400

    Add support for target folder building

--- src/main/frontend/gulpfile.js
+++ src/main/frontend/gulpfile.js
@@ -4,9 +4,9 @@ const gulp = require('gulp');
 const htmlbeautify = require('gulp-html-beautify');
 const sass = require('gulp-sass');
 const jshint = require('gulp-jshint');
-const image = require('gulp-image')
 const fileinclude = require('gulp-file-include');
 const webpack = require('webpack-stream');
+const fs = require('fs');
 
 const srcHTMLGlob = 'templates/**/*.html'
 const srcHTMLPartialsRoot = 'templates/partials';
@@ -15,44 +15,59 @@ const srcSASSGlob = 'sass/**/*.scss';
 const srcJSMain = 'js/main.js';
 const srcJSGlob = 'js/**/*.js';
 const srcImageGlob = 'img/**/*.{png,jpg,jpeg}';
-const distRoot = '../resources/static';
-const distCSS = '../resources/static/css'
-const distImage = '../resources/static/img';
-const distJS = '../resources/static/js';
-const distHTML = '../resources/templates';
+const resourceRoot = '../resources';
+const resourceHTML = `${resourceRoot}/templates`;
+const resourceStaticRoot = `${resourceRoot}/static`;
+const resourceCSS = `${resourceStaticRoot}/css`;
+const resourceImage = `${resourceStaticRoot}/img`;
+const resourceJS = `${resourceStaticRoot}/js`;
+const targetRoot = '../../../target/classes';
+const targetHTML = `${targetRoot}/templates`;
+const targetStaticRoot = `${targetRoot}/static`;
+const targetCSS = `${targetStaticRoot}/css`;
+const targetImage = `${targetStaticRoot}/img`;
+const targetJS = `${targetStaticRoot}/js`;
 const port = process.env.GULP_PORT || 3000;
-const isProduction = process.env.ENV == 'PROD';
 const usingThymeleaf = process.env.USING_THYMELEAF == 'true';
 
 gulp.task('html', () => {
-  return gulp.src(srcHTMLGlob)
+  let stream = gulp.src(srcHTMLGlob)
     .pipe(fileinclude({
       prefix: '@@',
-      basepath: srcHTMLPartialsRoot,
-      context: {
-        usingThymeleaf: isProduction || usingThymeleaf 
-      }
+      basepath: srcHTMLPartialsRoot
     }))
     .pipe(htmlbeautify())
-    .pipe(gulp.dest(distHTML))
+    .pipe(gulp.dest(resourceHTML));
+  
+  if (fs.existsSync(targetHTML)) {
+    stream.pipe(gulp.dest(targetHTML));
+  }
 });
 
 gulp.task('sass', () => {
-  return gulp.src(srcSASSMain)
+  let stream = gulp.src(srcSASSMain)
     .pipe(sass({ outputStyle: 'compressed' })
       .on('error', sass.logError))
-    .pipe(gulp.dest(distCSS));
+    .pipe(gulp.dest(resourceCSS));
+
+  if (fs.existsSync(targetCSS)) {
+    stream.pipe(gulp.dest(targetCSS));
+  }
 });
 
 gulp.task('js', () => {
-  return gulp.src(srcJSMain)
+  let stream = gulp.src(srcJSMain)
     .pipe(webpack({
       output: {
         filename: "bundle.js"
       },
       devtool: 'source-map',
     }))
-    .pipe(gulp.dest(distJS));
+    .pipe(gulp.dest(resourceJS));
+
+  if (fs.existsSync(targetJS)) {
+    stream.pipe(gulp.dest(targetJS));
+  }
 });
 
 gulp.task('lint', function () {
@@ -62,13 +77,11 @@ gulp.task('lint', function () {
 });
 
 gulp.task('image', () => {
-  if (isProduction) {
-    return gulp.src(srcImageGlob)
-      .pipe(image())
-      .pipe(gulp.dest(distImage));
-  } else {
-    return gulp.src(srcImageGlob)
-      .pipe(gulp.dest(distImage));
+  let stream = gulp.src(srcImageGlob)
+      .pipe(gulp.dest(resourceImage));
+  
+  if (fs.existsSync(targetImage)) {
+    stream.pipe(gulp.dest(targetImage));
   }
 });
 
@@ -79,17 +92,13 @@ gulp.task('watch', () => {
   gulp.watch(srcJSGlob, ['lint', 'js']);
 });
 
-if (isProduction) {
-  gulp.task('build', ['html', 'sass', 'js', 'image']);
-} else {
-  gulp.task('build', ['html', 'sass', 'lint', 'js', 'image']);
-}
+gulp.task('build', ['html', 'sass', 'lint', 'js', 'image']);
 
 gulp.task('serve', () => {
-   const app = require('./app');
-   app.listen(port, () => {
-      console.log(`Server listening on port ${port}`);
-   });
+  const app = require('./app');
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
 });
 
 gulp.task('default', ['build', 'serve']);
