c162f32230670a8c36782c59b1e93fbdcb2d71fa
Yaneury Fermin <fermin.yaneury@gmail.com>
2018-04-16 21:42:17 -0400

    Add lucene init on app startup

--- src/main/java/com/mashedtomatoes/App.java
+++ src/main/java/com/mashedtomatoes/App.java
@@ -1,14 +1,33 @@
 package com.mashedtomatoes;
 
+import javax.persistence.EntityManager;
+
+import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.jpa.Search;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.event.EventListener;
+
 
 @SpringBootApplication
 @EnableCaching
 public class App {
+  @Autowired EntityManager em;
 
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
+  }
+
+  @EventListener(ApplicationReadyEvent.class)
+  public void indexDatabase() {
+    System.out.println("Indexing db");
+    em = em.getEntityManagerFactory().createEntityManager();
+    try {
+      FullTextEntityManager fullTextEntityManager = Search.getFullTextEntityManager(em);
+      fullTextEntityManager.createIndexer().startAndWait();
+    } catch (InterruptedException ex) {}
   }
 }
--- src/main/java/com/mashedtomatoes/search/SearchViewController.java
+++ src/main/java/com/mashedtomatoes/search/SearchViewController.java
@@ -3,15 +3,12 @@ package com.mashedtomatoes.search;
 import com.mashedtomatoes.celebrity.Celebrity;
 import com.mashedtomatoes.celebrity.CelebrityService;
 import com.mashedtomatoes.media.*;
-import org.hibernate.search.jpa.FullTextEntityManager;
-import org.hibernate.search.jpa.Search;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.persistence.EntityManager;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,8 +20,6 @@ public class SearchViewController {
   @Autowired TVShowService tvShowService;
 
   @Autowired CelebrityService celebrityService;
-
-  @Autowired EntityManager em;
 
   @GetMapping("/search")
   public String getSearchResults(Model m, @RequestParam(value = "expr", required = false) String expr) {
@@ -42,12 +37,5 @@ public class SearchViewController {
     m.addAttribute("celebrities", celebrities);
 
     return "search/search";
-  }
-
-  @GetMapping("/initializeHibernateSearchLuceneIndex")
-  public String initializeLuceneIndex() throws InterruptedException {
-    FullTextEntityManager fullTextEntityManager = Search.getFullTextEntityManager(em);
-    fullTextEntityManager.createIndexer().startAndWait();
-    return "index";
   }
 }
