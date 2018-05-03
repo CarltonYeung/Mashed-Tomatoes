e006ecfb0f039e912793419003c44d928de12440
Carlton Yeung <viacarlton@gmail.com>
2018-05-03 10:03:18 -0400

    CriticRating same as AudienceRating. Leaving as-is because I don't want to change too much code and break things.

--- src/main/java/com/mashedtomatoes/rating/CriticRating.java
+++ src/main/java/com/mashedtomatoes/rating/CriticRating.java
@@ -1,30 +1,11 @@
 package com.mashedtomatoes.rating;
 
-import java.net.URL;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "CriticRatings")
 public class CriticRating extends Rating {
-  private String title;
-  private URL link;
-
-  public CriticRating() {}
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public void setLink(URL link) {
-    this.link = link;
-  }
-
-  public String getTitle() {
-    return title;
-  }
-
-  public URL getLink() {
-    return link;
+  public CriticRating() {
   }
 }
