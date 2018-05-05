750060d1beda50d96ece89fbd768cbb1fec28a43
Aarin Chase <aarin.chase@stonybrook.edu>
2018-05-05 01:28:50 -0400

    Created ReviewReport entity and the repository for ReviewReport entity.

---
+++ src/main/java/com/mashedtomatoes/rating/ReviewReportRepository.java
@@ -0,0 +1,7 @@
+package com.mashedtomatoes.rating;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReviewReportRepository extends CrudRepository<ReviewReport, Long> {
+
+}
